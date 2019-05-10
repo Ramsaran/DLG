1f24d0e646d5f5007b8c3b4375b38194846e9117
Ramsaran <Ramsarantester@gmail.com>
2019-05-10 11:53:52 +0100

    Add files via upload

---
+++ DLGFeature/DLGNavigation.feature
@@ -0,0 +1,11 @@
+﻿Feature: DLGNavigation and search vehicle reistration number
+	As a user
+	i want to enter the url and enter the vehicle registration number to search the validity
+
+@mytag
+Scenario: Successful Navigation and search the vehicle registration number 
+	Given I have launch the Browser
+	And I have entered URL
+	Then the homepage should be display
+	And Enter the vehicle registration number and click on Find vehicle button
+	Then Vehicle registration number should be display cover start and cover end
---
+++ Stepdefinitions/Navigation and Search.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace DLG.Stepdefinitions
+{
+    [Binding]
+    class Navigation_and_Search
+    {
+        public IWebDriver driver;
+        [Given(@"I have launch the Browser")]
+        public void GivenIHaveLaunchTheBrowser()
+        {
+            driver = new FirefoxDriver();
+        }
+
+        [Given(@"I have entered URL")]
+        public void GivenIHaveEnteredURL()
+        {
+            driver.Navigate().GoToUrl("https://covercheck.vwfsinsuranceportal.co.uk/");
+        }
+
+        [Then(@"the homepage should be display")]
+        public void ThenTheHomepageShouldBeDisplay()
+        {
+            true.Equals(driver.FindElement(By.Id("dlg-dealersearch-title")).Displayed);
+        }
+
+        [Then(@"Enter the vehicle registration number and click on Find vehicle button")]
+        public void ThenEnterTheVehicleRegistrationNumberAndClickOnFindVehicleButton()
+        {
+            driver.FindElement(By.Id("vehicleReg")).SendKeys("OV12UYY");
+            driver.FindElement(By.ClassName("track-search")).Click();
+        }
+
+        [Then(@"Vehicle registration number should be display cover start and cover end")]
+        public void ThenVehicleRegistrationNumberShouldBeDisplayCoverStartAndCoverEnd()
+        {
+            true.Equals(driver.FindElement(By.Id("dlg-dealersearch-title")).Displayed);
+        }
+
+    }
+}
